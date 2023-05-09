fddebc3667fca3c4bb5304bc815ae9d3d857f8c7
v1r <djmaddoxx1@gmail.com>
2023-05-09 11:38:13 +0200

    Update userbar.php

--- userbar.php
+++ userbar.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 header('Content-Type: image/png');
 
 function addText($im, $text, $font, $fontSize, $x, $y, $textColor, $strokeColor, $strokeWidth)
